36d1624b1ba4ebff5009675d04e25b5d57b5bbe7
radioactive-link <dunsethsamiam@gmail.com>
2025-02-19 20:55:07 -0500

    refactor: make other subsystems public

--- src/main/java/org/steeltalons/subsystems/ElevatorSubsystem.java
+++ src/main/java/org/steeltalons/subsystems/ElevatorSubsystem.java
@@ -2,7 +2,7 @@ package org.steeltalons.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-class ElevatorSubsystem extends SubsystemBase {
+public class ElevatorSubsystem extends SubsystemBase {
   public ElevatorSubsystem() {
 
   }
--- src/main/java/org/steeltalons/subsystems/IntakeSubsystem.java
+++ src/main/java/org/steeltalons/subsystems/IntakeSubsystem.java
@@ -2,7 +2,7 @@ package org.steeltalons.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-class IntakeSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
   public IntakeSubsystem() {
 
   }
