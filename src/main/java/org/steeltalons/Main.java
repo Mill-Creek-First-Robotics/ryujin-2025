98c2720809b63688b65b377ce36e9113782cc842
radioactive-link <dunsethsamiam@gmail.com>
2025-02-19 20:27:29 -0500

    chore: fmt

--- src/main/java/org/steeltalons/Main.java
+++ src/main/java/org/steeltalons/Main.java
@@ -7,7 +7,8 @@ package org.steeltalons;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
-  private Main() {}
+  private Main() {
+  }
 
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
