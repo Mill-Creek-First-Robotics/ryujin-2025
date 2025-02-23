bc77d268a5c78ff9f61e8604848ac0538be9b5eb
radioactive-link <dunsethsamiam@gmail.com>
2025-02-23 15:43:14 -0500

    refactor: add some drivetrain constants

--- src/main/java/org/steeltalons/Constants.java
+++ src/main/java/org/steeltalons/Constants.java
@@ -4,7 +4,14 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Mass;
+import edu.wpi.first.units.measure.MomentOfInertia;
 
 public class Constants {
   private Constants() {
@@ -42,6 +49,29 @@ public class Constants {
     private DrivetrainConstants() {
     }
 
+    public static final double kGearRatio = 10.71;
+    // [Dimensions]
+    public static final Distance kWheelDiameter = Units.Inches.of(6);
+    // horizontal distance from middle to a wheel in meters
+    public static final double kDriveTrainBaseX = .212725; // placeholder
+    // vertical distance from middle to a wheel in meters
+    public static final double kDrivetrainBaseY = .2667; // placeholder
+    public static final Translation2d[] kWheelOffsets = new Translation2d[] {
+        new Translation2d(kDriveTrainBaseX, kDrivetrainBaseY),
+        new Translation2d(kDriveTrainBaseX, -kDrivetrainBaseY),
+        new Translation2d(-kDriveTrainBaseX, kDrivetrainBaseY),
+        new Translation2d(-kDriveTrainBaseX, -kDrivetrainBaseY),
+    };
+    // [Physics stuff]
+    public static final Mass kMass = Units.Kilograms.of(34); // placeholder (taken from CAD)
+    public static final MomentOfInertia kMomentOfInertia = Units.KilogramSquareMeters.of(3.35); // placeholder
+    public static final double kWheelCoefficientOfFriction = .7;
+    public static final LinearVelocity kMaxSpeed = Units.FeetPerSecond.of(14.5); // placeholder
+    public static final MecanumDriveKinematics kDriveKinematics = new MecanumDriveKinematics(
+        kWheelOffsets[0],
+        kWheelOffsets[1],
+        kWheelOffsets[2],
+        kWheelOffsets[3]);
   } // end DrivetrainConstants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   public static class ElevatorConstants {
