e5f55710dbffdc745faadb1abb73a658b5f6a937
radioactive-link <dunsethsamiam@gmail.com>
2025-03-25 12:32:29 -0400

    refactor: enable telemetry for drivetrain

--- src/main/java/org/steeltalons/RobotContainer.java
+++ src/main/java/org/steeltalons/RobotContainer.java
@@ -5,6 +5,7 @@ import static org.steeltalons.Constants.kControllerPort;
 import org.steeltalons.subsystems.DriveSubsystem;
 import org.steeltalons.subsystems.RollerSubsystem;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -17,6 +18,8 @@ public class RobotContainer {
   public RobotContainer() {
     configureDefaultCommands();
     configureBindings();
+
+    SmartDashboard.putData(driveSubsystem);
   }
 
   private void configureDefaultCommands() {
